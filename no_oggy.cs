dd65fd119641908255bf6c85a6b0d6a0e2a4dda4
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-08-13 14:14:57 +0530

    Removing_oggy_via_tolist

--- no_oggy.cs
+++ no_oggy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 class NoOggyChecker
 {
-  static void RemoveOggy(List<string> names..ToList())
+  static void RemoveOggy(List<string> names.ToList())
   {
     foreach (var name in names)
     {
